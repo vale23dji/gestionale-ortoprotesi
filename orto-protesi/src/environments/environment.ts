7d0ac4147a398f6f74605b4f741cafc0181fed63
Valentin D. Bandiaky <djiggi23@gmail.com>
2025-09-20 11:20:22 +0200

    aggiunta possibilità di sviluppare in modalità debug/sviluppo accedendo al db senza errori di fetching

--- orto-protesi/src/environments/environment.ts
+++ orto-protesi/src/environments/environment.ts
@@ -1,10 +1,20 @@
 /**
  * Questo file contiene le variabili di ambiente per lo sviluppo locale.
  * Utilizziamo queste variabili durante il testing e lo sviluppo.
- */
+
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5233/api',
   uploadUrl: 'http://localhost:5233/uploads',
   jwtExpirationDate: 3600, // 1 ora
 };
+ */
+
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  apiUrl: '/api',
+  uploadUrl: '/uploads',
+  jwtExpirationDate: 3600,
+};
+
